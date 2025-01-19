e6148570d02fb2f0cc012f129637c559a220634c
israfilova14 <aynurisrafilova9@gmail.com>
2025-01-19 12:57:43 +0400

    some changes

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
 const connectDB = require('./config/db.js');
-const router = require('./routes/index.js');
 const app = express();
+const router = require('./routes/index.js');
+
  
- 
-// Ensure JSON parsing middleware is placed before routes
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
 }));
  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/api", router);
  
 // Server setup
